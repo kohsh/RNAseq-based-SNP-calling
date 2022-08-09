8bf9f3b9a623697126afd3fdbd014fa964e92b67
K-hsh <78065630+K-hsh@users.noreply.github.com>
2022-08-09 11:56:54 +1000

    Update variant-calling-GATK.sh

--- Scripts/variant-calling-GATK.sh
+++ Scripts/variant-calling-GATK.sh
@@ -7,7 +7,8 @@ gatk-4.2.3.0/gatk SplitNCigarReads -R ../References/GRCh38.primary_assembly.geno
 # 2b. BaseRecalibrator-GATK
 
 gatk-4.2.3.0/gatk BaseRecalibrator -R ../References/GRCh38.primary_assembly.genome.fa -I ${sample}_SplitNCigar.bam 
---use-original-qualities -O ${sample}_recalibration_report --known-sites ../References/Homo_sapiens_assembly38.dbsnp138.vcf --known-sites ../References/Homo_sapiens_assembly38.known_indels.vcf.gz
+--use-original-qualities -O ${sample}_recalibration_report --known-sites ../References/Homo_sapiens_assembly38.dbsnp138.vcf --known-sites 
+../References/Homo_sapiens_assembly38.known_indels.vcf.gz
 
 # 3b. ApplyBQSR-GATK
 
@@ -26,18 +27,3 @@ gatk-4.2.3.0/gatk HaplotypeCaller -R ../References/GRCh38.primary_assembly.genom
 -L 0004-scattered.interval_list -L 0005-scattered.interval_list -L 0006 scattered.interval_list -L 0007-scattered.interval_list 
 -L 0008-scattered.interval_list -L 0009-scattered.interval_list -O ${sample}.vcf.gz -dont-use-soft-clipped-bases 
 -stand-call-conf 20 --dbsnp ../References/Homo_sapiens_assembly38.dbsnp138.vcf --java-options '-DGATK_STACKTRACE_ON_USER_EXCEPTION=true'
-
-### Merge VCFs & Indexing ###
-
-# 6b. bcftools
-
-bcftools-1.9/bcftools merge -l list-of-selected-VCFs.txt -Oz -o selected.vcf.gz 
-& 
-tabix -p vcf selected.vcf.gz
-
-### Filteration of called SNPs ###
-
-# 7b. VariantFilteration-GATK
-
-gatk-4.2.3.0/gatk VariantFiltration -R ../References/GRCh38.primary_assembly.genome.fa -V selected.vcf.gz --window 35 --cluster 3 
---filter-name "FS" --filter "FS > 30.0" --filter-name "QD" --filter "QD < 2.0" -O selected_VF.vcf.gz
